cbd24865a4bcdfa923a6f7ae46fe1847fa233f2c
Jasmine Reisler <REISLER@ETSU.EDU>
2023-10-20 15:37:28 -0400

    Have been writing the function to do embedded formatted messages. Hopefully this will be useful in a scheduled anouncements and events section at some point

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 import json
+from discord import Embed
 
 #Initialize the bot. Utilizing commands just imported and discord package. Give prefects that the bot will listen out for on the discord sever. The bot detects prefect (e.g. !) and then looks for the command after. If user typed !hello, a programmed bot might type hi. ! says call and hello is the fucntion being called
 
@@ -55,5 +56,19 @@ async def on_member_remove(member): #When user leaves, will run this function
     channel = client.get_channel(1161381888078925926)
     await channel.send("Goodbye! Sad to see you leaving!")
 
+#Attempting embed anouncements. Set up so if user send a specfic message, the bot will detect the word dog and reply with an embed
+@client.command()
+async def embed(ctx):   #function named embed
+    #make sure url has http or https at the beginning with ://. 
+    embed = discord.Embed(title="Go Bucs!", url="https://www.etsu.edu/ehome/", description = "ETSU Homepage", color=0x0066cc) #Used W3Schools color picker. Need to add 0x before the hex
+    #author = ctx.author
+    embed.set_author(name=ctx.author.display_name, url="https://www.etsualumni.org/s/974/bp18/interior.aspx?sid=974&gid=1&pgid=1222", icon_url=ctx.author.avatar.url)
+    embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/0/0b/Appalachian_Mountains_wallpaper.jpg")
+    #name is just for title name, title of a field. 
+    embed.add_field(name="Fall 2023", value="Week 8", inline=TRUE)
+    #tell bot to send embed to chat
+    await ctx.send(embed=embed)
+    
+
 #Goes at the end
-client.run("MTE2MjEwNjE1NTc5NjAyMTQ3OQ.Ga7jN8.EgtDey9JZGKpLoHKLSmjM0e77FfSwFexigMU6g") #tells bot to run. Goes through script and then starts running it. Need token. Do NOT share TOKEN ever
+client.run("") #tells bot to run. Goes through script and then starts running it. Need token. Do NOT share TOKEN ever
